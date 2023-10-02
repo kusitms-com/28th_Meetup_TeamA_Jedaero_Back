148afe563a715519b1c1ce1701e00afc108b2f96
kimdohyung <pdohyung@gmail.com>
2023-10-02 23:28:24 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/jaedaero/JaedaeroApplication.java
@@ -0,0 +1,13 @@
+package com.jaedaero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaedaeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaedaeroApplication.class, args);
+	}
+
+}
