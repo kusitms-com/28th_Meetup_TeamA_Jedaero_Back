817d60500082721e7b35bc62cb184d9362161196
kimdohyung <pdohyung@gmail.com>
2023-11-13 21:45:45 +0900

    feat: 예외 처리 관련 클래스 추가 (#7)

---
+++ src/main/java/com/backend/error/exception/GlobalExceptionHandler.java
@@ -0,0 +1,53 @@
+package com.backend.error.exception;
+
+import com.backend.error.dto.ErrorResponse;
+import com.backend.error.exception.custom.BusinessException;
+import com.backend.error.exception.custom.TokenException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handle(final Exception e) {
+        log.error("서버 예외: {}", e);
+        return ResponseEntity.internalServerError().body(ErrorResponse.of(500, e.getMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handle(final BusinessException e) {
+        log.info("비즈니스 로직 예외: {}", e);
+        return ResponseEntity.status(e.getCode()).body(ErrorResponse.of(e.getCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handle(final TokenException e) {
+        log.info("토큰 예외: {}", e);
+        return ResponseEntity.status(e.getCode()).body(ErrorResponse.of(e.getCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handle(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        String firstErrorMessage = bindingResult.getFieldErrors().get(0).getDefaultMessage();
+
+        List<String> errorList = bindingResult.getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        log.warn("에러 리스트: {}", errorList);
+        return ResponseEntity.status(BAD_REQUEST).body(ErrorResponse.of(BAD_REQUEST.value(), firstErrorMessage));
+    }
+}
