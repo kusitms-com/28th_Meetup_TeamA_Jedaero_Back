4b9dc08c621cfb3a1077f046d3c93a81975ce48c
kimdohyung <pdohyung@gmail.com>
2023-11-16 18:11:37 +0900

    feat: JWT 의존성을 추가하고 리프레쉬 토큰 기능 추가 (#11)

--- src/main/java/com/backend/domain/auth/dto/request/JoinRequestDto.java
+++ src/main/java/com/backend/domain/auth/dto/request/JoinRequestDto.java
@@ -3,15 +3,16 @@ package com.backend.domain.auth.dto.request;
 import com.backend.domain.user.entity.GroupType;
 import com.backend.domain.user.entity.User;
 import jakarta.validation.constraints.NotEmpty;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public record JoinRequestDto(@NotEmpty String password, @NotEmpty String email, @NotEmpty String type,
                              @NotEmpty String typeName) {
-    public User toEntity() {
+    public User toEntity(PasswordEncoder passwordEncoder) {
         return User.builder()
                 .email(email)
-                .password(password)
+                .password(passwordEncoder.encode(this.password))
                 .type(GroupType.create(type))
-                .typeName(typeName) //todo. type를 enum 타입을 분리할 것
+                .typeName(typeName)
                 .build();
     }
 }
