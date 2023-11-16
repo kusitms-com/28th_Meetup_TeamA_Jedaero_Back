478987602c3f85d6f49823e53ef229bbb09f0fca
kimdohyung <pdohyung@gmail.com>
2023-11-16 18:21:00 +0900

    feat: 토큰을 검사하는 필터 추가 (#11)

---
+++ src/main/java/com/backend/jwt/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,57 @@
+package com.backend.jwt.filter;
+
+import com.backend.jwt.service.JwtProvider;
+import com.backend.jwt.service.ApiUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+    private final ApiUserDetailsService apiUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String token = jwtProvider.extractToken(request)
+                .filter(inputToken -> !jwtProvider.isExpired(inputToken))
+                .orElse(null);
+
+        log.info("토큰 : {}", token);
+        if (token != null) {
+            String username = jwtProvider.getUsernameFromToken(token);
+            log.info("토큰이 존재한다. username : {}", username);
+            try {
+                UserDetails userDetails = apiUserDetailsService.loadUserByUsername(username);
+
+                if (userDetails != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities());
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            } catch (AuthenticationException e) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+        }
+        filterChain.doFilter(request, response);
+    }
+}
