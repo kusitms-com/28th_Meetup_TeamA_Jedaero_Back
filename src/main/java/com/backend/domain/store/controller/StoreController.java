d8d149ca357913e59a10ed3bbe53df77671fed32
yeop0740 <ssucgoo@gmail.com>
2023-11-18 22:44:09 +0900

    feat: Swagger 계정 관련 객체 제외, 인증 헤더 추가

--- src/main/java/com/backend/domain/store/controller/StoreController.java
+++ src/main/java/com/backend/domain/store/controller/StoreController.java
@@ -6,9 +6,11 @@ import com.backend.domain.auth.dto.LoginUser;
 import com.backend.domain.store.dto.*;
 import com.backend.domain.store.service.StoreService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/store")
+@SecurityRequirement(name = "bearer-key")
 @Tag(name = "가게", description = "가게 관련 API")
 public class StoreController {
 
@@ -29,17 +32,17 @@ public class StoreController {
     }
 
     // TODO useremail을 바탕으로 university의 longitude, latitude 정보를 가져와서 이용한다.
-    @GetMapping("/details/{storeId}")
-    @Operation(
-            summary = "가게 상세 정보 조회",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            content = @Content(schema = @Schema(implementation = ReadStoreDetailsDto.class)))
-            })
-    public ResponseEntity<ReadStoreDetailsDto> readStoreDetails(@Login LoginUser loginUser, @PathVariable Long storeId) {
-        return ResponseDto.ok(storeService.readStoreDetails(loginUser, storeId));
-    }
+        @GetMapping("/details/{storeId}")
+        @Operation(
+                summary = "가게 상세 정보 조회",
+                responses = {
+                        @ApiResponse(
+                                responseCode = "200",
+                                content = @Content(schema = @Schema(implementation = ReadStoreDetailsDto.class)))
+                })
+        public ResponseEntity<ReadStoreDetailsDto> readStoreDetails(@Parameter(hidden = true) @Login LoginUser loginUser, @Parameter(name = "storeId") @PathVariable Long storeId) {
+            return ResponseDto.ok(storeService.readStoreDetails(loginUser, storeId));
+        }
 
     @GetMapping("/search")
     @Operation(
@@ -49,7 +52,7 @@ public class StoreController {
                             responseCode = "200",
                             content = @Content(schema = @Schema(implementation = ReadStoresDto.class)))
             })
-    public ResponseEntity<ReadStoresDto> readStores(@Login LoginUser loginUser, @ModelAttribute ReadStoresRequest request) {
+    public ResponseEntity<ReadStoresDto> readStores(@Parameter(hidden = true) @Login LoginUser loginUser, @ModelAttribute ReadStoresRequest request) {
         return ResponseDto.ok(storeService.readStores(loginUser, request));
     }
 
@@ -59,7 +62,7 @@ public class StoreController {
             responses = {
                     @ApiResponse(responseCode = "201")
             })
-    public ResponseEntity<Void> createPick(@Login LoginUser loginUser, @RequestBody PickRequest request) {
+    public ResponseEntity<Void> createPick(@Parameter(hidden = true) @Login LoginUser loginUser, @RequestBody PickRequest request) {
         storeService.createPick(loginUser, request);
         return ResponseDto.created();
     }
@@ -70,7 +73,7 @@ public class StoreController {
             responses = {
                     @ApiResponse(responseCode = "200")
             })
-    public ResponseEntity<Void> deletePick(@Login LoginUser loginUser, @RequestBody PickRequest request) {
+    public ResponseEntity<Void> deletePick(@Parameter(hidden = true) @Login LoginUser loginUser, @RequestBody PickRequest request) {
         storeService.deletePick(loginUser, request);
         return ResponseDto.ok();
     }
