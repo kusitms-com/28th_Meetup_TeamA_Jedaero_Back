b7b5e99a811baea4aa1f239c5bdcdf90c5d1e350
kimdohyung <pdohyung@gmail.com>
2023-11-20 23:37:06 +0900

    feat: 사용자가 등록한 팝업을 가져오는 쿼리문 추가 (#21)

--- src/main/java/com/backend/domain/user/repository/UserRepository.java
+++ src/main/java/com/backend/domain/user/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package com.backend.domain.user.repository;
 
+import com.backend.domain.popup.domain.Popup;
 import com.backend.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,4 +16,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByEmail(String email);
 
     Optional<User> findByRefreshToken(String refreshToken);
+
+    @Query("SELECT p FROM Popup p JOIN p.contract c JOIN c.university u JOIN u.user user WHERE user.id = :userId")
+    Page<Popup> findPopupsByUserId(@Param("userId") Long userId, Pageable pageable);
 }
