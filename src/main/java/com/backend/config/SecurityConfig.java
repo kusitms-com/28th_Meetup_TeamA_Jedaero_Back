15d6f84c440d18824ef3915ec808a0db0f4900fc
kimdohyung <pdohyung@gmail.com>
2023-11-22 13:12:46 +0900

    docs: 대학 목록 조회 api swagger 문서 추가 (#26)

--- src/main/java/com/backend/config/SecurityConfig.java
+++ src/main/java/com/backend/config/SecurityConfig.java
@@ -63,6 +63,7 @@ public class SecurityConfig implements WebMvcConfigurer {
                                 .requestMatchers(mvcMatcherBuilder.pattern("/mail/**")).permitAll()
                                 .requestMatchers(mvcMatcherBuilder.pattern("/s3/create")).permitAll()
                                 .requestMatchers(mvcMatcherBuilder.pattern("/sms")).permitAll()
+                                .requestMatchers(mvcMatcherBuilder.pattern("/univ/**")).permitAll()
                                 .anyRequest().authenticated())
                 .exceptionHandling()
                 .authenticationEntryPoint(entryPoint);
