e11a2c64a27b673d49943faeeed5ded2c65a7357
kimdohyung <pdohyung@gmail.com>
2023-11-23 02:05:55 +0900

    refactor: 대학 목록 조회시 id도 반환하도록 수정 (#36)

--- src/main/java/com/backend/domain/university/dto/response/getUnivResponseDto.java
+++ src/main/java/com/backend/domain/university/dto/response/getUnivResponseDto.java
@@ -2,8 +2,8 @@ package com.backend.domain.university.dto.response;
 
 import com.backend.domain.university.entity.University;
 
-public record getUnivResponseDto(String univName, String email) {
+public record getUnivResponseDto(Long universityId, String univName, String email) {
     public static getUnivResponseDto from(University university) {
-        return new getUnivResponseDto(university.getName(), university.getEmail());
+        return new getUnivResponseDto(university.getUniversityId(), university.getName(), university.getEmail());
     }
 }
