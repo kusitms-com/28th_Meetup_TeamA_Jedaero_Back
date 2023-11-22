ee8b14ea9cf0d9b31707c94414d6a1711f81d107
yeop0740 <ssucgoo@gmail.com>
2023-11-22 19:26:23 +0900

    feat: 스웨거 적용(#29)

--- src/main/java/com/backend/domain/event/controller/EventController.java
+++ src/main/java/com/backend/domain/event/controller/EventController.java
@@ -5,7 +5,9 @@ import com.backend.domain.auth.dto.Login;
 import com.backend.domain.auth.dto.LoginUser;
 import com.backend.domain.event.dto.*;
 import com.backend.domain.event.service.EventService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -22,24 +24,33 @@ public class EventController {
     private final EventService eventService;
 
     @PostMapping
+    @Operation(
+            summary = "이벤트 등록",
+            description =
+                    "<p>쿠폰과 스탬프를 등록합니다</p>" +
+                            "<p>쿠폰을 등록할 때에는 type을 COUPON으로 선택하고 startDate와 duration을 입력하지 않는다.</p>" +
+                            "<p>스탬프를 등록할 때에는 type을 STAMP 선택하고 startDate와 duration을 입력한다. discount는 입력하지 않는다.</p>")
     public ResponseEntity<Void> createEvent(@Parameter(hidden = true) @Login LoginUser loginUser, @RequestBody CreateEventRequest request) {
         eventService.createEvent(loginUser, request);
         return ResponseDto.created();
     }
 
     @GetMapping("/coupon")
+    @Operation(summary = "쿠폰 목록 조회")
     public ResponseEntity<ReadCouponsDto> readCoupons(@Parameter(hidden = true) @Login LoginUser loginUser, @ModelAttribute ReadEventsRequest request) {
         ReadCouponsDto events = eventService.readCoupons(loginUser, request);
         return ResponseDto.ok(events);
     }
 
     @GetMapping
+    @Operation(summary = "이벤트 목록 조회")
     public ResponseEntity<ReadEventsDto> readEvents(@Parameter(hidden = true) @Login LoginUser loginUser, @ModelAttribute ReadEventsRequest request) {
         ReadEventsDto events = eventService.readEvents(loginUser, request);
         return ResponseDto.ok(events);
     }
 
     @PatchMapping
+    @Operation(summary = "이벤트 수정")
     public ResponseEntity<Void> updateEvent(@Parameter(hidden = true) @Login LoginUser loginUser, @RequestBody UpdateEventRequest request) {
         eventService.updateEvent(loginUser, request);
         return ResponseDto.ok();
@@ -47,6 +58,7 @@ public class EventController {
 
 
     @DeleteMapping("/{eventId}")
+    @Operation(summary = "이벤트 삭제")
     public ResponseEntity<Void> deleteEvent(@Parameter(hidden = true) @Login LoginUser loginUser, @Parameter(name = "eventId") @PathVariable Long eventId) {
         eventService.deleteEvent(loginUser, eventId);
         return ResponseDto.ok();
