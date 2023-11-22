4c0ef849035a57bacdb4486518534547a09caf22
yeop0740 <ssucgoo@gmail.com>
2023-11-22 22:44:09 +0900

    fix: 이벤트 생성 파라미터 유효성 추가

--- src/main/java/com/backend/domain/event/dto/CreateEventRequest.java
+++ src/main/java/com/backend/domain/event/dto/CreateEventRequest.java
@@ -5,6 +5,7 @@ import com.backend.domain.event.entity.Condition;
 import com.backend.domain.event.entity.Event;
 import com.backend.domain.popup.domain.EndDateType;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,12 +19,15 @@ import java.util.List;
 public class CreateEventRequest {
 
     @Schema(example = "3")
+    @NotEmpty
     private Long storeId;
 
     @Schema(example = "COUPON")
+    @NotEmpty
     private BenefitType type;
 
     @Schema(example = "5000원 할인 쿠폰")
+    @NotEmpty
     private String name;
 
     private List<String> conditions;
@@ -39,17 +43,37 @@ public class CreateEventRequest {
     @Schema(example = "일주일간")
     private EndDateType duration;
 
+    {
+        startDate = LocalDate.now();
+    }
+
     public Event toEntity() {
+        validateDuration();
+        validateQuantity();
         Event event = Event.builder()
                 .type(type)
                 .name(name)
                 .quantity(createDiscount())
-                .discount(discount)
+                .discount(quantity)
                 .startDate(startDate)
-                .endDate(startDate.plusDays(duration.getPlusDate()))
+                .endDate(createEndDate())
                 .build();
         event.addAll(createConditions());
         return event;
+    }
+
+    private void validateDuration() {
+        if (type.equals(BenefitType.STAMP)) {
+            if (duration == null) {
+                throw new RuntimeException();
+            }
+        }
+    }
+
+    public void validateQuantity() {
+        if (quantity <= 0 && quantity != -1) {
+            throw new RuntimeException();
+        }
     }
 
     private int createDiscount() {
@@ -63,6 +87,13 @@ public class CreateEventRequest {
         return conditions.stream()
                 .map(Condition::new)
                 .toList();
+    }
+
+    private LocalDate createEndDate() {
+        if (duration == null) {
+            return startDate;
+        }
+        return startDate.plusDays(duration.getPlusDate());
     }
 
 }
