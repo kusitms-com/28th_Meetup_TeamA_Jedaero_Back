a0aa01be3f4837e26053ac435ed8df46f15f1b17
kimdohyung <pdohyung@gmail.com>
2023-11-23 12:43:08 +0900

    fix: allowCredentials 추가로 인한 allowedOrigins 수정 (#40)

--- src/main/java/com/backend/config/SecurityConfig.java
+++ src/main/java/com/backend/config/SecurityConfig.java
@@ -85,7 +85,7 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000", "https://api.jedero.site", "https://jedero.site")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .exposedHeaders("*")
